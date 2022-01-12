c4b67743d1bfd24e439d67bcd23e2c580e22660e
Leonardo Nunes <zleo465@gmail.com>
2022-01-12 01:48:23 -0300

    created the structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {FiSearch} from "react-icons/fi"
+import "./styles.css"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+      <div className="container-input">
+        <input 
+        type="text"
+        placeholder="Digite seu CEP..."/>
+
+        <button className="button-search">
+          <FiSearch size={25} color="black" /> 
+        </button>
+      </div>
+      <main className="main">
+        <h2>CEP: 32532535</h2>
+        <span>Rua</span>
+        <span>Complemento:</span>
+        <span>vila rosa</span>
+        <span>Campo grande</span>
+      </main>
     </div>
   );
 }
